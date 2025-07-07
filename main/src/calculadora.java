37943a1f2fa5e8b5caf4d72187342a5c56497f1e
MATHEUS AUGUSTO GASPARETTO <matheusgaspa98@gmail.com>
2025-07-07 20:52:05 -0300

    adição de funções

---
+++ main/src/calculadora.java
@@ -0,0 +1,12 @@
+public class calculadora {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int soma = (a + b);
+        int multi = (a * b);
+
+        System.out.println(" A soma é: " + soma);
+        System.out.println(" A multiplicação é: ");
+    }
+}
